ff53aa0042e00de0a703fea3cb9625c1ed63bb3c
xumeivs <737436809@qq.com>
2019-02-20 15:24:38 +0800

    first commit

--- 降维.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from sklearn.decomposition import PCA
-import pandas
-data=pandas.read_csv('C:/Users/Administrator/Desktop/DATA/luqu.csv')
-pca1=PCA(2)
-pca1.fit(data[1:4])
-tz1=pca1.components_
-rate=pca1.explained_variance_ratio_
-reduction=pca1.transform(data)
